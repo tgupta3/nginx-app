f1418b0dfe56bff6a7fa02a7d9f961125692fb42
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-06-05 18:57:47 +0000

    Fixed typ

--- scripts/config_packages.sh
+++ scripts/config_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mdkir -p /home/ec2-user/sample_app
+mkdir -p /home/ec2-user/sample_app
 curl -sS https://getcomposer.org/installer -o /home/ec2-user/sample_app/composersetup.php 
 php /home/ec2-user/sample_app/composersetup.php --install-dir=/home/ec2-user/sample_app/
 php /home/ec2-user/sample_app/composer.phar require aws/aws-sdk-php  --working-dir=/home/ec2-user/sample_app/
