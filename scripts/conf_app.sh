628616c8b8628d251ae9c6f19a6c2d772aa2d60e
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-06-05 17:37:45 +0000

    8th commit

--- scripts/conf_app.sh
+++ scripts/conf_app.sh
@@ -5,5 +5,5 @@ rm -rf /etc/php-fpm.d/www.conf
 mv  /tmp/php.ini /etc/
 mv /tmp/www.conf /etc/php-fpm.d/ 
 mkdir /home/ec2-user/nginx_server
-mv -r /tmp/nginx_server/* /home/ec2-user/nginx_server/ 
+mv /tmp/nginx_server/* /home/ec2-user/nginx_server/ 
 chown -R ec2-user:ec2-user /home/ec2-user/nginx_server
