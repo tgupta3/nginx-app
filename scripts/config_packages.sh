d2d05994c6cbf3061270d658951f9816219814e9
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-06-05 18:47:32 +0000

    Nth commit

--- scripts/config_packages.sh
+++ scripts/config_packages.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 mdkir -p /home/ec2-user/sample_app
-alias cdcompo="cd /home/ec2-user/sample_app"
-cdcompo
-curl -sS https://getcomposer.org/installer | php
-php composer.phar require aws/aws-sdk-php
-php composer.phar require gilbitron/php-simplecache
+curl -sS https://getcomposer.org/installer -o /home/ec2-user/sample_app/composersetup.php 
+php /home/ec2-user/sample_app/composersetup.php --install-dir=/home/ec2-user/sample_app/
+php /home/ec2-user/sample_app/composer.phar require aws/aws-sdk-php  --working-dir=/home/ec2-user/sample_app/
+php /home/ec2-user/sample_app/composer.phar require gilbitron/php-simplecache --working-dir=/home/ec2-user/sample_app/
 rm -rf /home/ec2-user/sample_app/vendor/gilbitron/cache
 mkdir -p /home/ec2-user/sample_app/vendor/gilbitron/cache/
 touch /home/ec2-user/sample_app/vendor/gilbitron/cache/label.cache
-chown -R ec2-user:ec2-user /home/ec2-user/sample_app/vendor/gilbitron/cache/
+chown -R ec2-user:ec2-user /home/ec2-user/sample_app
