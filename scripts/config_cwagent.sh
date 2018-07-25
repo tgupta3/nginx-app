1fa50a82697fa0b69cb4ba54ee2823012ddccb48
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-07-25 15:29:49 +0000

    Updated directory for CW install

--- scripts/config_cwagent.sh
+++ scripts/config_cwagent.sh
@@ -2,7 +2,7 @@
 
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
 unzip AmazonCloudWatchAgent.zip -d cwagent/
-./cwagent/install.sh
+cd cwagent/ && ./install.sh
 mv /tmp/cw_config.json /opt/aws/amazon-cloudwatch-agent/bin/
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/cw_config.json -s
 
