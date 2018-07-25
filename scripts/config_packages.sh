251646507c99533bd9e116365a0da7e1b847c117
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-07-25 18:49:24 +0000

    Fix for memory issues on composer

--- scripts/config_packages.sh
+++ scripts/config_packages.sh
@@ -5,6 +5,7 @@ curl -sS https://getcomposer.org/installer -o /home/ec2-user/sample_app/composer
 php /home/ec2-user/sample_app/composersetup.php --install-dir=/home/ec2-user/sample_app/
 php /home/ec2-user/sample_app/composer.phar require aws/aws-sdk-php  --working-dir=/home/ec2-user/sample_app/
 php /home/ec2-user/sample_app/composer.phar require gilbitron/php-simplecache --working-dir=/home/ec2-user/sample_app/
+php -d memory_limit=-1 /home/ec2-user/sample_app/composer.phar update --working-dir=/home/ec2-user/sample_app/
 rm -rf /home/ec2-user/sample_app/vendor/gilbitron/cache
 mkdir -p /home/ec2-user/sample_app/vendor/gilbitron/cache/
 touch /home/ec2-user/sample_app/vendor/gilbitron/cache/label.cache
