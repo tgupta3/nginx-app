a5b0a2ddaa75dc79b3750afd546e531bc884b07d
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-08-13 18:36:17 +0000

    Removed dependencies

--- scripts/conf_nginx.sh
+++ scripts/conf_nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 rm -rf /etc/nginx/nginx.conf
-mv /tmp/nginx.conf /etc/nginx/
+mv /tmp/nginx.conf /etc/nginx/ -f
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-yum install -y nginx
-yum install -y php php-fpm php-mysql
+#yum install -y nginx
+#yum install -y php php-fpm php-mysql
 easy_install pip==9.0.3
 yum install -y python-pip
 pip install flask
