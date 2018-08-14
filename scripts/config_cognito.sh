97298fc4a8a8864e4b94e19ae369a96864016fe7
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-08-14 00:24:38 +0000

    Updated cognito file bug

--- scripts/config_cognito.sh
+++ scripts/config_cognito.sh
@@ -2,6 +2,7 @@
 set -e
 
 mkdir /home/ec2-user/python_app -p 
-mv -f /tmp/python_app/* /home/ec2-user/python_app
+cp -r /tmp/python_app/* /home/ec2-user/python_app
+rm -R /tmp/python_app/*
 sudo service supervisord stop
 sudo service supervisord start
