f867b28e77e6c19a6d2b667a2849e841ed1e89de
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-08-14 00:20:02 +0000

    Dummy comment

--- scripts/config_cognito.sh
+++ scripts/config_cognito.sh
@@ -2,5 +2,6 @@
 set -e
 
 mkdir /home/ec2-user/python_app -p 
-mv /tmp/python_app/* /home/ec2-user/python_app
+mv -f /tmp/python_app/* /home/ec2-user/python_app
+sudo service supervisord stop
 sudo service supervisord start
