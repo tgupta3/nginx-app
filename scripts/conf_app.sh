ca31e854f08ea46624cc86ab27d454502d2f48bc
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-08-14 00:01:30 +0000

    Updated mkdir permissions

--- scripts/conf_app.sh
+++ scripts/conf_app.sh
@@ -4,6 +4,6 @@ rm -rf /etc/php.ini
 rm -rf /etc/php-fpm.d/www.conf
 mv  /tmp/php.ini /etc/
 mv /tmp/www.conf /etc/php-fpm.d/
-mkdir /home/ec2-user/nginx_server
+mkdir /home/ec2-user/nginx_server -p
 mv /tmp/nginx_server/* /home/ec2-user/nginx_server/
 chown -R ec2-user:ec2-user /home/ec2-user/nginx_server
--- scripts/config_cognito.sh
+++ scripts/config_cognito.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-mkdir /home/ec2-user/python_app
+mkdir /home/ec2-user/python_app -p 
 mv /tmp/python_app/* /home/ec2-user/python_app
 sudo service supervisord start
