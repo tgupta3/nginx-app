f7ffe2dfd2ebcba958888a1c54f3d5c2913cb353
Ubuntu <ubuntu@ip-192-168-100-4.ec2.internal>
2018-11-21 18:03:10 +0000

    Fixed directory exist issue

--- scripts/config_zsh_usr.sh
+++ scripts/config_zsh_usr.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -d "/home/ec2-user/.oh-my-zsh" ]; then
+  rm -rf /home/ec2-user/.oh-my-zsh
+fi
+
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh 
 git clone https://github.com/bhilburn/powerlevel9k.git /home/ec2-user/.oh-my-zsh/custom/themes/powerlevel9k
 rm -rf .zshrc
